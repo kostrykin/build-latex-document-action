cf7bc2a9532f36d2de41f83c40a3b5d3a3bf362c
Leonid Kostrykin <void@evoid.de>
2024-09-05 18:15:02 +0200

    Create build_index.py

---
+++ build_index.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+
+names = list()
+
+for filepath in glob.glob('**/*', recursive=True):
+    if os.path.isfile(filepath):
+        filepaths.append(filepath)
+
+html = """
+<html>
+<body>
+<ul>
+""" + '\n'.join(f'<li><a href="{filepath}">{filepath}</a></li>' for filepath in sorted(filepath)) + """
+</ul>
+</body>
+"""
+
+with open('index.html', 'w') as fout:
+    fout.write(html)
